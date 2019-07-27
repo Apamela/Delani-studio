56634da19d89c430eb623600d821e44173b5f579
pamela sindy <pamelab.desire@gmail.com>
2019-07-27 18:16:38 +0200

    add the styles in css

--- css/script.js
+++ css/script.js
@@ -3,5 +3,24 @@ $(document).ready(function () {
         $("#mell").toggle();
         $("#mella").toggle();
     });
-
-}
+    $("#mell").click(function () {
+        $("#mella").toggle();
+        $("#mell").toggle();
+    });
+    $("#pella").click(function () {
+        $("#pell").toggle();
+        $("#pella").toggle();
+    });
+    $("#pell").click(function () {
+        $("#pella").toggle();
+        $("#pell").toggle();
+    });
+    $("#patte").click(function () {
+        $("#patt").toggle();
+        $("#patte").toggle();
+    });
+    $("#patt").click(function () {
+        $("#patte").toggle();
+        $("#patt").toggle();
+    });
+});
