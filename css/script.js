ce82902c9f7ac819a5bb97a04cb795b3a61a9bb6
pamela sindy <pamelab.desire@gmail.com>
2019-07-29 11:20:15 +0200

    add the condition in js

--- css/script.js
+++ css/script.js
@@ -23,14 +23,57 @@ $(document).ready(function () {
         $("#patte").toggle();
         $("#patt").toggle();
     });
-});
-// function message() {
-//     alert("the massage")
 
-// }
-$("#cena").mouseover(function () {
-    $("#comp").show();
-});
-$("#cena").mouseleave(function () {
-    $("#comp").hide();
+    // function message() {
+    //     alert("the massage")
+
+    // }
+    $("#cena").mouseover(function () {
+        $("#comp").show();
+    });
+    $("#cena").mouseleave(function () {
+        $("#comp").hide();
+    });
+    $("#son").mouseover(function () {
+        $("#kell").show();
+    });
+    $("#son").mouseleave(function () {
+        $("#kell").hide();
+    });
+    $("#sen").mouseover(function () {
+        $("#mim").show();
+    });
+    $("#sen").mouseleave(function () {
+        $("#mim").hide();
+    });
+    $("#pam").mouseover(function () {
+        $("#mom").show();
+    });
+    $("#pam").mouseleave(function () {
+        $("#mom").hide();
+    });
+    $("#lolo").mouseover(function () {
+        $("#receive").show();
+    });
+    $("#lolo").mouseleave(function () {
+        $("#receive").hide();
+    });
+    $("#up").mouseover(function () {
+        $("#send").show();
+    });
+    $("#up").mouseleave(function () {
+        $("#send").hide();
+    });
+    $("#sendy").mouseover(function () {
+        $("#oll").show();
+    });
+    $("#sendy").mouseleave(function () {
+        $("#oll").hide();
+    });
+    $("#pop").mouseover(function () {
+        $("#lli").show();
+    });
+    $("#pop").mouseleave(function () {
+        $("#lli").hide();
+    });
 });
